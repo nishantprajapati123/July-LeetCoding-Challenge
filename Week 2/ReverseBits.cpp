0feacc57ad5505fb8b4272596fe79fc162be5865
nishantprajapati123 <knishant147@gmail.com>
2020-07-12 16:16:26 +0530

    Week 2

---
+++ Week 2/ReverseBits.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        
+        int res = 0;
+        for(int i=0;i<32;i++)
+        {
+            if(n & (1ll<<i))
+                res = res | (1ll<<(32-i-1));
+        }
+        return res;
+    }
+};

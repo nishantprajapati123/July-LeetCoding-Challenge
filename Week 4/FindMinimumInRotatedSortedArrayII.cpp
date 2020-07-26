b31cf54f52f0f338af0e55669481c94d814aca35
nishantprajapati123 <knishant147@gmail.com>
2020-07-26 23:54:02 +0530

    Week 4

---
+++ Week 4/FindMinimumInRotatedSortedArrayII.cpp
@@ -0,0 +1,17 @@
+// Time complexity :- O(log(n))
+// Space complexity :- O(1)
+class Solution {
+    public:
+    int findMin(vector<int>& nums) {
+        int l = 0;
+        int r = nums.size()- 1;
+        while (l < r) {
+            int mid = l + (r - l) / 2;
+            if (nums[mid] > nums[r]) l = mid + 1;
+            else if (nums[mid] < nums[r]) r = mid;
+            else r--;
+        }
+
+        return nums[l];
+    }
+};
